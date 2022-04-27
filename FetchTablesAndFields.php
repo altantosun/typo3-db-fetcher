3b1d2cfb4b1fb5e310cbffbd66e3b7ed80014fe4
Altan Tosun <altantosun@gmail.com>
2022-04-27 17:32:59 +0200

    Adds functions

---
+++ FetchTablesAndFields.php
@@ -0,0 +1,39 @@
+
+    public static function fetchFieldsFromTable(string $tableName): array
+    {
+
+        /** @var ConnectionPool $connectionPool */
+        $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+        $queryBuilder = $connectionPool->getConnectionForTable($tableName);
+        $tableFields = $queryBuilder->executeQuery('SHOW FIELDS FROM ' . $tableName)->fetchAllAssociative();
+
+        $tableFieldNames = [];
+
+        foreach ($tableFields as $tableField) {
+            $tableFieldNames[] = $tableField['Field'];
+        }
+
+        return $tableFieldNames;
+    }
+
+    /**
+     * @return array<string>
+     * @throws \Doctrine\DBAL\DBALException|\Doctrine\DBAL\Driver\Exception
+     */
+    public static function fetchTableNames(): array
+    {
+        /** @var ConnectionPool $connectionPool */
+        $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+        $queryBuilder = $connectionPool->getConnectionForTable('pages'); // just name one table name, usually pages always exists
+        $tables = $queryBuilder->executeQuery('SHOW TABLES')->fetchAllAssociative();
+
+        $tableNames = [];
+
+        foreach ($tables as $table) {
+            foreach ($table as $item) {
+                $tableNames[] = $item;
+            }
+        }
+
+        return $tableNames;
+    }
